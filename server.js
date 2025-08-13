cdfae9094d788a9e410543332b236d0dced04799
Spryzenweb <123577747+Spryzenweb@users.noreply.github.com>
2025-08-13 19:46:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ function calculateWinnings(winningNumber) {
 }
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; 
 server.listen(PORT, () => {
     console.log(`Sunucu ${PORT} portunda çalışıyor.`);
     console.log("Oyun admin panelinden manuel olarak kontrol edilecek.");
